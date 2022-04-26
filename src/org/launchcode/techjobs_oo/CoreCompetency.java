327e45277a7050bd8e41da412caecabefa157d5d
Su Kang <sukang2017@u.northwestern.edu>
2022-04-25 22:50:54 -0500

    equals and hashCode completed in PositionType class

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -7,6 +7,7 @@ public class CoreCompetency {
     private static int nextId = 1;
     private String value;
 
+
     public CoreCompetency() {
         this.id = nextId;
         nextId++;
@@ -39,4 +40,21 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
